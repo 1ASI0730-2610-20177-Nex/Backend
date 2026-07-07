d42728aba8454713f90380482995acd3fdec331e
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-07-07 10:44:20 -0500

    feat: add iam domain module

---
+++ Electro.Corporation.Platform/Iam/Domain/Model/Aggregates/UserAudit.cs
@@ -0,0 +1,9 @@
+﻿using Electro.Corporation.Platform.Shared.Domain.Model.Entities;
+
+namespace Electro.Corporation.Platform.Iam.Domain.Model.Aggregates;
+
+public partial class User : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
---
+++ Electro.Corporation.Platform/Iam/Domain/Model/Queries/GetUserByUsernameQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace Electro.Corporation.Platform.Iam.Domain.Model.Queries;
+
+public record GetUserByUsernameQuery(string Username);
