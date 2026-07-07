8de73f448793fa828afa4c4dfc71b1bb338bd370
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-07-07 10:41:37 -0500

    feat: add profiles infrastructure module

---
+++ Electro.Corporation.Platform/Profiles/Infrastructure/Persistence/EntityFrameworkCore/Repositories/ProfileRepository.cs
@@ -0,0 +1,24 @@
+﻿using Electro.Corporation.Platform.Profiles.Domain.Model.Aggregates;
+using Electro.Corporation.Platform.Profiles.Domain.Model.ValueObjects;
+using Electro.Corporation.Platform.Profiles.Domain.Repositories;
+using Electro.Corporation.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using Electro.Corporation.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electro.Corporation.Platform.Profiles.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+/// <summary>
+///     Profile repository implementation
+/// </summary>
+/// <param name="context">
+///     The database context
+/// </param>
+public class ProfileRepository(AppDbContext context)
+    : BaseRepository<Profile>(context), IProfileRepository
+{
+    /// <inheritdoc />
+    public async Task<Profile?> FindProfileByEmailAsync(EmailAddress email, CancellationToken cancellationToken)
+    {
+        return await Context.Set<Profile>().FirstOrDefaultAsync(p => p.Email == email, cancellationToken);
+    }
+}
