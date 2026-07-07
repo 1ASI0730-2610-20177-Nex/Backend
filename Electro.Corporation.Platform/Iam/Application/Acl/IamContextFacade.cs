a965238b2080496a3d8acb981ff1c8aff7cd41cf
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-07-07 10:45:08 -0500

    feat: add iam application module

---
+++ Electro.Corporation.Platform/Iam/Application/Acl/IamContextFacade.cs
@@ -0,0 +1,35 @@
+﻿using Electro.Corporation.Platform.Iam.Application.CommandServices;
+using Electro.Corporation.Platform.Iam.Application.QueryServices;
+using Electro.Corporation.Platform.Iam.Domain.Model.Commands;
+using Electro.Corporation.Platform.Iam.Domain.Model.Queries;
+using Electro.Corporation.Platform.Iam.Interfaces.Acl;
+
+namespace Electro.Corporation.Platform.Iam.Application.Acl;
+
+public class IamContextFacade(IUserCommandService userCommandService, IUserQueryService userQueryService)
+    : IIamContextFacade
+{
+    public async Task<int> CreateUser(string username, string password, CancellationToken cancellationToken)
+    {
+        var signUpCommand = new SignUpCommand(username, password);
+        var signUpResult = await userCommandService.Handle(signUpCommand, cancellationToken);
+        if (signUpResult.IsFailure) return 0;
+        var getUserByUsernameQuery = new GetUserByUsernameQuery(username);
+        var result = await userQueryService.Handle(getUserByUsernameQuery, cancellationToken);
+        return result?.Id ?? 0;
+    }
+
+    public async Task<int> FetchUserIdByUsername(string username, CancellationToken cancellationToken)
+    {
+        var getUserByUsernameQuery = new GetUserByUsernameQuery(username);
+        var result = await userQueryService.Handle(getUserByUsernameQuery, cancellationToken);
+        return result?.Id ?? 0;
+    }
+
+    public async Task<string> FetchUsernameByUserId(int userId, CancellationToken cancellationToken)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var result = await userQueryService.Handle(getUserByIdQuery, cancellationToken);
+        return result?.Username ?? string.Empty;
+    }
+}
